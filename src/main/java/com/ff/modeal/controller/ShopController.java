e22d52db21867a9835f409ac8ed7233e1248d096
motherfather <father@mail.com>
2017-01-17 18:56:29 +0900

    json 데이터 주고 받기...

--- src/main/java/com/ff/modeal/controller/ShopController.java
+++ src/main/java/com/ff/modeal/controller/ShopController.java
@@ -1,10 +1,14 @@
 package com.ff.modeal.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,19 +33,21 @@ public class ShopController {
 	}
 	
 //	@Admin
-	@RequestMapping("/{no}")
+	@RequestMapping(value="/{no}", method=RequestMethod.GET)
 	public String view(@PathVariable("no") Long no, Model model) { // 매장 상세정보용으로 jsp에서 no값을 받아서 db에서 불러온다
 		model.addAttribute("view", shopService.view(no));
 		return "shop/shopView";
 	}
 	
-	@RequestMapping("/test")
 	@ResponseBody
-	public JSONResult test(@RequestParam(value="keyword", required=false, defaultValue="") String keyword,
-											@RequestParam(value="filterCheck", required=false, defaultValue="1") Integer option,
-											@RequestParam(value="page", required=false, defaultValue="1") Integer page) {
+	@RequestMapping(value="/test", method=RequestMethod.POST)
+	public JSONResult test(@RequestBody(required=false) Map<String, String> map) {
+		System.out.println("받아오는 값 : " + map);
+
+		Integer page = Integer.parseInt(map.get("page"));
+		Integer option = Integer.parseInt(map.get("filterCheck"));
+		String keyword = map.get("keyword");
 		
 		return JSONResult.success(shopService.list(page, keyword, option));
 	}
-	
 }
