fcc112d4402d4004ab5357859813f718241bb4b3
cho <you-smile@nate.com>
2017-01-23 13:17:57 +0900

    안드로이드로부터 회원가입 데이터 불러오기 완료

--- src/main/java/com/ff/modeal/controller/app/UserControllerTest.java
+++ src/main/java/com/ff/modeal/controller/app/UserControllerTest.java
@@ -25,11 +25,24 @@ public class UserControllerTest {
 	public JSONResult InputString(@RequestParam(value="id", required=true, defaultValue="" ) String id,
 									@RequestParam(value="password", required=true, defaultValue="" ) String password,
 									@RequestParam(value="gender", required=true, defaultValue="" ) String gender,
-									@RequestParam(value="managerIdentified", required=true, defaultValue="1") String managerIdentified) {
+									@RequestParam(value="location", required=true, defaultValue="" ) String location,
+									@RequestParam(value="birth", required=true, defaultValue="" ) String birth,
+									@RequestParam(value="managerIdentified", required=true, defaultValue="1") String managerIdentified,
+									@RequestParam(value="mkName", required=true, defaultValue="") String mkName,
+									@RequestParam(value="mkAddress", required=true, defaultValue="") String mkAddress,
+									@RequestParam(value="mkPhoneN", required=true, defaultValue="") String mkPhoneN,
+									@RequestParam(value="mkIntro", required=true, defaultValue="") String mkIntro) {
 		System.out.println(id);
 		System.out.println(password);
 		System.out.println(gender);
+		System.out.println(location);
+		System.out.println(birth);
 		System.out.println(managerIdentified);
+		
+		System.out.println(mkName);
+		System.out.println(mkAddress);
+		System.out.println(mkPhoneN);
+		System.out.println(mkIntro);
 		
 		return JSONResult.success(1);
 	}
