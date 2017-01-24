8e672881b128ef26177ff456c0e6011d2f08b5ef
kyj <BIT@DESKTOP-DOGVD1T>
2017-01-24 13:13:10 +0900

    1,GSON 제거 2,DaumSearchService 간략화

--- src/main/java/com/ff/modeal/vo/app/map/ChannelVo.java
+++ src/main/java/com/ff/modeal/vo/app/map/ChannelVo.java
@@ -7,100 +7,93 @@ import java.util.List;
  */
 
 public class ChannelVo {
-    private int totalCount;
-    private String link;
-    private int result;
-    private String generator;
-    private int pageCount;
-    private String lastBuildDate;
-    private List<ItemVo> item;
-    private String title;
-    private String description;
+	private String totalCount;
+	private String link;
+	private String result;
+	private String generator;
+	private String pageCount;
+	private String lastBuildDate;
+	private List<ItemVo> item;
+	private String title;
+	private String description;
 
-    public int getTotalCount() {
-        return totalCount;
-    }
+	public String getTotalCount() {
+		return totalCount;
+	}
 
-    public void setTotalCount(int totalCount) {
-        this.totalCount = totalCount;
-    }
+	public void setTotalCount(String totalCount) {
+		this.totalCount = totalCount;
+	}
 
-    public String getLink() {
-        return link;
-    }
+	public String getLink() {
+		return link;
+	}
 
-    public void setLink(String link) {
-        this.link = link;
-    }
+	public void setLink(String link) {
+		this.link = link;
+	}
 
-    public int getResult() {
-        return result;
-    }
+	public String getResult() {
+		return result;
+	}
 
-    public void setResult(int result) {
-        this.result = result;
-    }
+	public void setResult(String result) {
+		this.result = result;
+	}
 
-    public String getGenerator() {
-        return generator;
-    }
+	public String getGenerator() {
+		return generator;
+	}
 
-    public void setGenerator(String generator) {
-        this.generator = generator;
-    }
+	public void setGenerator(String generator) {
+		this.generator = generator;
+	}
 
-    public int getPageCount() {
-        return pageCount;
-    }
+	public String getPageCount() {
+		return pageCount;
+	}
 
-    public void setPageCount(int pageCount) {
-        this.pageCount = pageCount;
-    }
+	public void setPageCount(String pageCount) {
+		this.pageCount = pageCount;
+	}
 
-    public String getLastBuildDate() {
-        return lastBuildDate;
-    }
+	public String getLastBuildDate() {
+		return lastBuildDate;
+	}
 
-    public void setLastBuildDate(String lastBuildDate) {
-        this.lastBuildDate = lastBuildDate;
-    }
+	public void setLastBuildDate(String lastBuildDate) {
+		this.lastBuildDate = lastBuildDate;
+	}
 
-    public List<ItemVo> getItem() {
-        return item;
-    }
+	public List<ItemVo> getItem() {
+		return item;
+	}
 
-    public void setItem(List<ItemVo> item) {
-        this.item = item;
-    }
+	public void setItem(List<ItemVo> item) {
+		this.item = item;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    @Override
-    public String toString() {
-        return "ChannelVo{" +
-                "totalCount=" + totalCount +
-                ", link='" + link + '\'' +
-                ", result=" + result +
-                ", generator='" + generator + '\'' +
-                ", pageCount=" + pageCount +
-                ", lastBuildDate='" + lastBuildDate + '\'' +
-                ", item=" + item +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "ChannelVo [totalCount=" + totalCount + ", link=" + link + ", result=" + result + ", generator="
+				+ generator + ", pageCount=" + pageCount + ", lastBuildDate=" + lastBuildDate + ", item=" + item
+				+ ", title=" + title + ", description=" + description + "]";
+	}
+
 }
