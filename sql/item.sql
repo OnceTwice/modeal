fb1db8c7fa1b866b93808d7ab3253824bd0d2f4a
kangyeojeong <yeo8841@gmail.com>
2017-02-09 21:40:12 +0900

    20170209 아이템 sql문 수정

--- sql/item.sql
+++ sql/item.sql
@@ -1,11 +1,19 @@
 select * from shop;
 
+insert into shop (address, newaddress, name, phone, picture, introduce, longitude, latitude)
+			VALUES('서울시 관악구 신림동', '서울시 관악구 신림로66길', 'GS25', '02-123-4567', 'shop2.jpg', '안녕하세요. GS25 신림로점입니다.', 30, 38); 
+
+delete FROM item WHERE no=3;
+
 select * from item; 
 
-insert into item (name, count, `oriPrice`, price, discount, `expDate`, picture, `shopNo`, `itemCategoryNo`)
-VALUES('딸기케이크', 1, 10000, 5000, 50, now(), 'im.jpg', 1, 2);
+insert into item (name, count, `oriPrice`, price, discount, `expDate`, picture, showItem, `shopNo`, `itemCategoryNo`)
+VALUES('컵라면 신라면', 1, 1000, 800, 20, now(), 'item3.jpg', 1, 2, 1);
 
 select * from itemcategory;
-insert INTO itemcategory (content) VALUES('케이크');
+
+insert INTO itemcategory (content) VALUES('밥');
+insert INTO itemcategory (content) VALUES('빵');
+insert INTO itemcategory (content) VALUES('음료');
 
 commit;
