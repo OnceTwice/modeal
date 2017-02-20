8b8a0beb7cc05c87e49b51a412d359201b10fdb6
cho <you-smile@nate.com>
2017-02-20 10:16:46 +0900

    충돌해결

--- src/main/java/com/ff/modeal/service/app/CommentService.java
+++ src/main/java/com/ff/modeal/service/app/CommentService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ff.modeal.vo.app.CommentVo;
 
-@Service
+@Service("commentappservice")
 public class CommentService {
 	public List<CommentVo> getAllComments() {
 		List<CommentVo> list = new ArrayList<CommentVo>();
