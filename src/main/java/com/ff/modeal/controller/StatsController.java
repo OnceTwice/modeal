0c4a076a849646f1f116bd84302f91440a331d1b
Your Name <you@example.com>
2017-02-23 20:40:54 +0900

    0223 통계 , item 수정

--- src/main/java/com/ff/modeal/controller/StatsController.java
+++ src/main/java/com/ff/modeal/controller/StatsController.java
@@ -1,11 +1,14 @@
 package com.ff.modeal.controller;
 
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.stereotype.*;
-import org.springframework.ui.*;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
 
-import com.ff.modeal.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ff.modeal.service.StatsService;
 
 @Controller
 @RequestMapping("/stats")
@@ -16,11 +19,11 @@ public class StatsController {
 	
 	@RequestMapping("")
 	public String statsIndex(Model model){
-//		List<Map<String, Object>> genderlist = statsService.getgenderCount();
-//		List<Map<String, Object>> agelist = statsService.getAgeCount();
+		List<Map<String, Object>> genderlist = statsService.getgenderCount();
+		List<Map<String, Object>> agelist = statsService.getAgeCount();
 		
-//		model.addAttribute("genderlist",genderlist);
-//		model.addAttribute("agelist",agelist);
+		model.addAttribute("genderlist",genderlist);
+		model.addAttribute("agelist",agelist);
 		
 		int man = statsService.getCountMan();
 		int woman = statsService.getCountWoman();
--- src/main/java/com/ff/modeal/controller/app/ItemController.java
+++ src/main/java/com/ff/modeal/controller/app/ItemController.java
@@ -6,9 +6,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ff.modeal.dto.JSONResult;
@@ -36,12 +36,16 @@ public class ItemController {
 	//검색결과 리스트 출력
 	@ResponseBody
 	@RequestMapping(value="/resultsearch", method=RequestMethod.POST)
-	public Object resultSearchList(@RequestBody(required=true)ItemVo itemVo){
+	public Object resultSearchList(
+			@RequestParam(value = "name", required = true, defaultValue = "") String name,
+			@RequestParam(value = "latitude", required = true, defaultValue = "") String latitude,
+			@RequestParam(value = "longitude", required = true, defaultValue = "") String longitude
+			){
 		
 	
-		List<Map<String, Object>> map = itemService.GetResultList(itemVo.getName()); 
+		List<Map<String, Object>> map = itemService.GetResultList(name,latitude, longitude); 
 
-		
+		System.out.println("-"+name+"-"+ latitude+"-"+ longitude);
 		return JSONResult.success(map);
 	}
 }
